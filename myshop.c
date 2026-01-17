e9f91f8fd560c1e2e2314bc324a1fd8c3018eb42
abdulqadirbca <abdulqadir6335@gmail.com>
2026-01-17 12:51:35 +0530

    mobile shop practice

---
+++ myshop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int mobileprice =15000;
+    float percentage = 10.5;
+    char series = 's';
+ printf("mobile price: %d\n",mobileprice);
+ printf("percentage: %f\n",percentage);
+ printf("series: %c\n",series);
+ return 0;
+}
