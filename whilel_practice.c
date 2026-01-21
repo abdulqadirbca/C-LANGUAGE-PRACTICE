e41b10b57d9d0c798fca42d7ef41b19d129aa1ea
abdulqadirbca <abdulqadir6335@gmail.com>
2026-01-21 09:59:00 +0530

    Create whilel_practice.c

---
+++ whilel_practice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int secret = 7;
+   int user = 0;
+   while (user !=secret){
+      printf("sahi numbe: ");
+      scanf("%d",&user);
+   }
+      printf("mubarak ho! sahi hai\n");
+return 0;
+}
+
