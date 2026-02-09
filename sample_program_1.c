f74c3895378e196e78619c4d49dfb411de7d978e
abdulqadirbca <abdulqadir6335@gmail.com>
2026-02-09 15:08:19 +0530

    Create sample_program_1.c

---
+++ sample_program_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+int a ,b; //creat two variables
+printf("enter number: "); // two numbers input from the user
+scanf("%d %d",&a ,&b);
+if(a==b){ // if else logic
+    printf("equal");
+} else {
+    printf("not equal");
+}
+    return 0;
+}
