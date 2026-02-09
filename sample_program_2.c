1f931b7f5876f2f6b106b6b842a4632a08454092
abdulqadirbca <abdulqadir6335@gmail.com>
2026-02-09 15:17:23 +0530

    Create sample_program_2.c

---
+++ sample_program_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+int a ; //creat variable
+printf("enter number: "); // one number input from the user
+scanf("%d",&a );
+if(a%2==0){ // if else logic
+    printf("even number");
+} else {
+    printf("odd number");
+}
+    return 0;
+}
