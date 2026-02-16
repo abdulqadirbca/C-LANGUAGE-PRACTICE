7b33f1e3c596ff8e240c9f73a17574810934932f
abdulqadirbca <abdulqadir6335@gmail.com>
2026-02-16 10:52:21 +0530

    Create loop_program_1_program1.c

---
+++ loop_program_1_program1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int n, i; // creat variable
+  printf("enter number ");//input from user
+  scanf("%d",&n);
+  for(i=1; i<=n; i++)
+  {printf("natural number= %d\n",i);}
+  return 0;
+}
